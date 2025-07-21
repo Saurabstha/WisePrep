fbadc02a4b7d1f27123f346fcd468f381341336c
Saurab Shrestha <saurab.shrestha99@gmail.com>
2025-07-21 09:28:26 +0545

    Remove console log.

--- firebase/client.ts
+++ firebase/client.ts
@@ -14,8 +14,6 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-console.log("FIREBASE CONFIG:", firebaseConfig);
-
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
