0f1c2481cd1e0b4a1f1577405b142c6c58fd6c0e
Saurab Shrestha <saurab.shrestha99@gmail.com>
2025-07-21 09:27:31 +0545

    Update env

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,15 +3,15 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBWTZKrWGdJyRuVR7sEzm4rPqbT1zSlAR0",
-  authDomain: "wiseprep-ecb8f.firebaseapp.com",
-  projectId: "wiseprep-ecb8f",
-  storageBucket: "wiseprep-ecb8f.firebasestorage.app",
-  // storageBucket: "wiseprep-ecb8f.appspot.com",
-  messagingSenderId: "55626230538",
-  appId: "1:55626230538:web:2360f19cac47a8c5c3a8f3",
-  measurementId: "G-MBNNCWRJ6H",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 console.log("FIREBASE CONFIG:", firebaseConfig);
